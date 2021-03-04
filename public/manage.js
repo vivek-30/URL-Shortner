ea19a9fc3454e54a3112f5d3455983d481c53263
vivek singh <vikky30singh@gmail.com>
2021-03-04 10:01:33 +0530

    redirect to new window instead of current one

--- public/manage.js
+++ public/manage.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
     // focus on main input
     $('#URL').focus(); 
 
+    // update number of clicks on UI by reloading the page.
+    // $.ajax({
+    //     type: 'GET',
+    //     url: '/:link',
+    //     success: function(){
+    //         location.href = '/';
+    //         location.reload();
+    //     }
+    // });
+
     $('#clear').click((e)=>{
         $.ajax({
-            type:'GET',
-            url:'/links/clear',
-            success:function(){
+            type: 'GET',
+            url: '/links/clear',
+            success: function(){
                 location.reload();
             }
         });
